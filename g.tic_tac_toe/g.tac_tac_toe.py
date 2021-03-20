097338907cc713de072d0be2adb9d461b38fc997
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-03-20 13:46:39 -0400

    defined the printBoard method, it print the layout of the tic-tak-toe

--- g.tic_tac_toe/g.tac_tac_toe.py
+++ g.tic_tac_toe/g.tac_tac_toe.py
@@ -2,7 +2,17 @@
 theBoard= { '7':' ','8':' ','9':' ',
             '4':' ','5':' ','6':' ',
             '1':' ','2':' ','3':' '}
+
 board_keys =[]
 
 for i in theBoard:
     board_keys.append(i)
+
+
+def printBoard(board):
+    print(board['7']+'|'+board['8']+'|'+board['9'])
+    print('-+-+-')
+    print(board['4']+'|'+board['5']+'|'+board['6'])
+    print('-+-+-')
+    print(board['1']+'|'+board['2']+'|'+board['3'])
+
